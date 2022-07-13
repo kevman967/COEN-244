b53f3858e1c51bdc6269f4d03953ea5c7475ec3d
kevman967 <kevman966@gmail.com>
2022-07-12 21:43:07 -0400

    master

--- Assignment#1/Question#1/NodeList.cpp
+++ Assignment#1/Question#1/NodeList.cpp
@@ -14,32 +14,43 @@ NodeList::~NodeList(){
 }
 void NodeList::print(){
     for(int i =0; i<=numItems;i++){
-        cout<<items->get_id()<<items->get_data();
-
+        cout <<"Node: "<<items[i].get_id()<<" Data: "<<items[i].get_data()<<endl;
     }
 }
-bool NodeList::check_node(Node n){
-
+bool NodeList::check_Node(int id1,int data1){
+    for(int i=0;i<numItems;i++){
+        if(items[i].get_id()==id1)
+            return true;
+        if(items[i].get_data()==data1)
+            return true;
+    }
+    return false;
 }
 //getter
 size_t NodeList::get_LIST_SIZE(){
     return LIST_SIZE;
 }
-Node NodeList::get_items(){
-    return *items;
-}
+
 size_t NodeList::get_numItems(){
     return numItems;
 }
-//setters
-/*
-void NodeList::set_LIST_SIZE(size_t n){
-    
+void NodeList::add_Node(Node& a){
+    if(numItems<LIST_SIZE){
+        items[numItems]=a;
+        numItems++;
+        cout<<"Node "<<a.get_id()<<" has been added to Nodelist\n";
+    }
 }
-*/
-void NodeList::set_items(Node n){
-  *items = n;  
-}
-void NodeList::set_numItems(size_t n){
-   numItems = n; 
+void NodeList::delete_Node(int node1){
+    for (int i=0;i<numItems;i++){
+        if(items[i].get_id()==node1){
+            for(int j=i;j<numItems-1;j++){
+                items[j] = items[j+1];
+            }
+            numItems--;
+            cout<<"Node "<<node1<<" has been deleted from NodeList\n";
+            return;
+        }
+    }
+    cout<<"Node "<<node1<<" cannot be found\n";
 }
